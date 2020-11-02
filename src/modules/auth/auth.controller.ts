42c7d00b840351102cba5044cb0986842eaeb0cc
virzs <2369310227@qq.com>
2020-11-02 22:31:56 +0800

    增加参数验证文件

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -8,6 +8,7 @@ import {
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { LoginDTO } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -16,14 +17,13 @@ export class AuthController {
   // 登录测试
   @UseGuards(AuthGuard('local'))
   @Post('login')
-  async login(@Body() body) {
+  async login(@Body() body: LoginDTO) {
     return this.authService.login(body);
   }
   // 测试登录后才可访问的接口，在需要的地方使用守卫，可保证必须携带token才能访问
   @UseGuards(AuthGuard('jwt'))
   @Get('me')
   getProfile(@Request() req) {
-    console.log('req', req);
     return req.user;
   }
 }
---
+++ src/modules/auth/dto/login.dto.ts
@@ -0,0 +1,8 @@
+import { IsString } from 'class-validator';
+
+export class LoginDTO {
+  @IsString()
+  username: string;
+  @IsString()
+  password: string;
+}
