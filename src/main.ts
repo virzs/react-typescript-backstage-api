fcb2a374cb34aee9746a2815ba11b222fff255dc
virzs <2369310227@qq.com>
2020-11-04 16:39:13 +0800

    对接swagger文档

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const configService = new ConfigService(`${process.env.NODE_ENV}.env`);
   const app = await NestFactory.create(AppModule, {
     logger: new Logger(),
   });
+  const swaggerOptions = new DocumentBuilder()
+
+    // DocumentBuilder是一个辅助类，有助于结构的基本文件SwaggerModule。它包含几种方法，可用于设置诸如标题，描述，版本等属性。
+    .setTitle('个人主页API文档') //文档标题
+    .setDescription('个人主页API文档') //文档介绍
+    .setVersion('1.0') //文档版本
+    .addBearerAuth()
+    .build();
+  // 为了创建完整的文档（具有定义的HTTP路由），我们使用类的createDocument()方法SwaggerModule。此方法带有两个参数，分别是应用程序实例和基本Swagger选项。
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  // 最后一步是setup()。它依次接受（1）装入Swagger的路径，（2）应用程序实例, （3）描述Nest应用程序的文档。
+  SwaggerModule.setup('doc', app, document);
   app.setGlobalPrefix('api');
   await app.listen(configService.get('APP_PORT') || 3000);
 }
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -12,12 +12,18 @@ import {
 } from '@nestjs/common';
 import { DetailDTO } from './dto/detail.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiTags, ApiQuery, ApiBody } from '@nestjs/swagger';
 
+@ApiTags('用户')
 @Controller('user')
 export class UserController {
   constructor(private readonly UserService: UserService) {}
   @Get()
   @UsePipes(DefaultDTOValidationPipe)
+  @ApiQuery({
+    name: 'id',
+    description: '传入用户id',
+  })
   byId(@Query() query: DetailDTO) {
     return this.UserService.byId(query);
   }
@@ -28,6 +34,10 @@ export class UserController {
   }
   @Post('register')
   @UsePipes(DefaultDTOValidationPipe)
+  @ApiBody({
+    type: Object,
+    description: '传入参数',
+  })
   register(@Body() body: RegisterDTO) {
     return this.UserService.register(body);
   }
