5975845f5700ff9f4da92ff15e4ad4ef60a9d504
virzs <2369310227@qq.com>
2020-11-09 23:19:19 +0800

    增加登录参数说明

--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDTO {
+  @ApiProperty({ example: 'account', description: '账号' })
   @IsString()
   @IsNotEmpty({ message: '账号不能为空' })
   account: string;
 
+  @ApiProperty({ example: 'password', description: '密码' })
   @IsString()
   @IsNotEmpty({ message: '密码不能为空' })
   password: string;
