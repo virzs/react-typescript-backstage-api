942175d8f298113f03cf8cab4ec6fcd37c20c398
virzs <2369310227@qq.com>
2020-11-15 23:02:34 +0800

    调整结构，删除多余文件

--- src/modules/auth/constants.ts
+++
@@ -1,7 +0,0 @@
-import { EnvConfigService } from 'src/config/env/env.config.service';
-
-const envConfigService = new EnvConfigService(`${process.env.NODE_ENV}.env`);
-
-export const jwtConstants = {
-  secret: envConfigService.get('JWT_SECRET'),
-};
