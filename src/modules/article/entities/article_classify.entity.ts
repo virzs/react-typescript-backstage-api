d539682da556fe84410cfd23a4fd6502e348a9b6
virzs <2369310227@qq.com>
2020-11-22 23:20:49 +0800

    文章分类实体

---
+++ src/modules/article/entities/article_classify.entity.ts
@@ -0,0 +1,41 @@
+import { CreateAndUpdateTimeEntity } from 'src/common/entities/createAndUpdateTime.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Article extends CreateAndUpdateTimeEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'varchar',
+    length: 20,
+    comment: '文章分类名',
+  })
+  name: string;
+  @Column({
+    type: 'varchar',
+    length: 30,
+    comment: '别名',
+  })
+  alias: string;
+
+  @Column({
+    type: 'varchar',
+    length: 100,
+    comment: '分类描述',
+  })
+  introduction: string;
+
+  @Column({
+    type: 'int',
+    default: 1,
+    comment: '分类等级',
+  })
+  level: number;
+
+  @Column({
+    type: 'varchar',
+    comment: '父级分类id',
+  })
+  parentId: string;
+}
