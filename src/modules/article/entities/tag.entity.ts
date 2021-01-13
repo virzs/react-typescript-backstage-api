a7c938b4f8ce7ffcdb0a7da524c0da51d9ecd9a9
virzs <2369310227@qq.com>
2021-01-13 17:36:26 +0800

    文章标签数据库设计

---
+++ src/modules/article/entities/tag.entity.ts
@@ -0,0 +1,31 @@
+import { CreateAndUpdateTimeEntity } from 'src/common/entities/createAndUpdateTime.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Article_Tag extends CreateAndUpdateTimeEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'varchar',
+    length: 20,
+    comment: '标签名称',
+  })
+  name: string;
+
+  @Column({
+    type: 'varchar',
+    length: 20,
+    comment: '标签别名',
+    default: null,
+  })
+  alias: string;
+
+  @Column({
+    type: 'varchar',
+    length: 100,
+    comment: '分类描述',
+    default: null,
+  })
+  introduction: string;
+}
