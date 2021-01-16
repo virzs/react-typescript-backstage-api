ee31b7f711afd4e9de21ad246c85bae39115f07e
virzs <2369310227@qq.com>
2021-01-16 23:29:39 +0800

    文章新增，编辑，删除，分页接口

--- src/modules/article/controllers/article.controller.ts
+++ src/modules/article/controllers/article.controller.ts
@@ -1,6 +1,48 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ArticleService } from './../services/article.service';
+import { Result } from 'src/common/interface/result.interface';
+import JwtAuthGuard from 'src/modules/auth/guards/jwtAuth.guard';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('文章')
 @Controller('article/article')
-export class ArticleController {}
+export class ArticleController {
+  constructor(private readonly articleService: ArticleService) {}
+
+  @Post('add')
+  @ApiOperation({ summary: '新增文章' })
+  @UseGuards(JwtAuthGuard)
+  add(@Body() body): Promise<Result> {
+    return this.articleService.add(body);
+  }
+
+  @Put('edit')
+  @ApiOperation({ summary: '编辑文章' })
+  @UseGuards(JwtAuthGuard)
+  edit(@Body() body): Promise<Result> {
+    return this.articleService.edit(body);
+  }
+
+  @Delete('delete')
+  @ApiOperation({ summary: '删除文章' })
+  @UseGuards(JwtAuthGuard)
+  delete(@Body() body): Promise<Result> {
+    return this.articleService.delete(body);
+  }
+
+  @Get('page')
+  @ApiOperation({ summary: '文章分页' })
+  @UseGuards(JwtAuthGuard)
+  page(@Query() query): Promise<Result> {
+    return this.articleService.page(query);
+  }
+}
